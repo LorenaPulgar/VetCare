256fbb0f8000cbff96138f13eff0970082eda000
Juan Diego Marin Soler <juands1903@gmail.com>
2023-04-21 21:34:54 -0500

    [FIX] Fixed a lot of bugs with the integration of firebase

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'bootstrap': 'bootstrap/dist/js/bootstrap.js'
+    }
+  }
+};
