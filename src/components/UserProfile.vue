e6ef7db40acf4e16c285bcc805bad449e2d3647e
Juan Diego Marin Soler <juands1903@gmail.com>
2023-04-25 22:17:09 -0500

    [UPD] update menu profile 'n configuration firestore

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+      <h2 class="mb-4">User Profile</h2>
+      <form>
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" class="form-control" v-model="name" />
+        </div>
+  
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" class="form-control" v-model="email" />
+        </div>
+  
+        <button type="submit" class="btn btn-primary" @click.prevent="submitForm">Save</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      name: 'UserProfile',
+      data() {
+        return {
+          name: '',
+          email: '',
+        };
+      },
+    
+      methods: {
+        submitForm() {
+          // Aquí puedes hacer una petición para enviar los datos del usuario al servidor y guardarlos.
+        },
+      },
+    };
+  </script>
+  
+  <style scoped>
+  .container {
+    max-width: 500px;
+    margin: auto;
+  }
+  
+  .form-group {
+    margin-bottom: 1.5rem;
+  }
+  
+  .btn-primary {
+    background-color: #007bff;
+    border-color: #007bff;
+  }
+  
+  .btn-primary:hover {
+    background-color: #0069d9;
+    border-color: #0062cc;
+  }
+  </style>  
